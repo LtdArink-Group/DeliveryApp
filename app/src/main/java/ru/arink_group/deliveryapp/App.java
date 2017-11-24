54fbd0a92dc920eb2886d762a2278e7927446911
kirillvs <kirillvs@mail.ru>
2017-11-24 12:07:23 +1000

    view created

--- app/src/main/java/ru/arink_group/deliveryapp/App.java
+++ app/src/main/java/ru/arink_group/deliveryapp/App.java
@@ -28,7 +28,7 @@ public class App extends Application {
     }
 
     public static String getCompanyId() {
-        return "10";
+        return "12";
     }
 
     public static String getUUID() {
